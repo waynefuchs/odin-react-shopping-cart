a4f6d8f2a419830444a285897d74daa88ea6a933
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-10-14 20:52:42 -0400

    Landing page not centering

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,11 +1,12 @@
-// import React from 'react';
 import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
     <div id="LandingPage">
-      <div>Your Search Begins</div>
-      <button>Here</button>
+      <div className="SearchBegins">
+        <div>Your Search Begins</div>
+        <button>Here</button>
+      </div>
     </div>
   );
 };
