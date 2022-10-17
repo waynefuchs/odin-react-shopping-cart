5ca473213982dd275d20b55ac91e79e51628ebd1
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-10-17 17:39:45 -0400

    Get LandingPage test passing

--- src/components/LandingPage.test.js
+++ src/components/LandingPage.test.js
@@ -1,8 +1,13 @@
+import {BrowserRouter as Router} from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import LandingPage from './LandingPage';
 
+function renderWithProviders(el) {
+  render(<Router>{el}</Router>);
+}
+
 test('Landing page exists', () => {
-  render(<LandingPage />);
+  renderWithProviders(<LandingPage />);
   const linkElement = screen.getByText(/your search begins/i);
   expect(linkElement).toBeInTheDocument();
 });
