804a4d6ecc981c93cd3d114c5ffcd1a9cebb8fba
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-10-17 22:05:03 -0400

    Implement link test

--- src/components/LandingPage.test.js
+++ src/components/LandingPage.test.js
@@ -1,13 +1,22 @@
-import {BrowserRouter as Router} from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { getAllByRole, render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom";  // optional
+import {MemoryRouter as Router} from 'react-router-dom';
+
 import LandingPage from './LandingPage';
 
 function renderWithProviders(el) {
-  render(<Router>{el}</Router>);
+  return render(<Router>{el}</Router>);
 }
 
 test('Landing page exists', () => {
   renderWithProviders(<LandingPage />);
   const linkElement = screen.getByText(/your search begins/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+it('should have a link in a modal to the store.', async () => {
+  const {getByRole} = renderWithProviders(<LandingPage />);
+  const elements = screen.getAllByRole('link').filter(e => e.textContent.match(/here/i));
+  expect(elements.length).toEqual(1);
 });
