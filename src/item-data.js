05fba1ed1a9de753042ab9daddeadcc55a3f440f
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-10-18 18:08:47 -0400

    Change id to static number
    This is what would be found in a database.

--- src/item-data.js
+++ src/item-data.js
@@ -11,21 +11,21 @@ import trainPlate from "./img/items/train-plate.png";
 import windmillPlate from "./img/items/windmill-plate.png";
 import womanCarving from "./img/items/woman-carving.png";
 
-const createItemObject = (name, image, shortDescription, price) => {
-  return { id: uniqid(), name, image, shortDescription, price };
+const createItemObject = (id, name, image, shortDescription, price) => {
+  return { id, name, image, shortDescription, price };
 };
 
 const data = [
-  createItemObject("Silk Road Camel", camel, "Silk Road Camel", 123.45),
-  createItemObject("Maine Lighthouse", lighthouse, "Maine Lighthouse", 234.56),
-  createItemObject("Wooden Loon Carving", loon, "Wooden Loon Carving", 300.50),
-  createItemObject("Swedish Man Carving", manCarving, "Swedish Man Carving", 222.22),
-  createItemObject("Swedish Woman Carving", womanCarving, "Swedish Woman Carving", 222.22),
-  createItemObject("Crystal Moose", mooseGlass, "Crystal Moose", 73.45),
-  createItemObject("Oriental Chalice", orientalCup, "Oriental Chalice", 500.00),
-  createItemObject("Silver Tea Cup", silverTeaCup, "Silver Tea Cup", 125.50),
-  createItemObject("Train Plate", trainPlate, "Train Plate", 80.80),
-  createItemObject("Windmill Plate", windmillPlate, "Windmill Plate", 80.80),
+  createItemObject(1, "Silk Road Camel", camel, "Silk Road Camel", 123.45),
+  createItemObject(2, "Maine Lighthouse", lighthouse, "Maine Lighthouse", 234.56),
+  createItemObject(3, "Wooden Loon Carving", loon, "Wooden Loon Carving", 300.50),
+  createItemObject(4, "Swedish Man Carving", manCarving, "Swedish Man Carving", 222.22),
+  createItemObject(5, "Swedish Woman Carving", womanCarving, "Swedish Woman Carving", 222.22),
+  createItemObject(6, "Crystal Moose", mooseGlass, "Crystal Moose", 73.45),
+  createItemObject(7, "Oriental Chalice", orientalCup, "Oriental Chalice", 500.00),
+  createItemObject(8, "Silver Tea Cup", silverTeaCup, "Silver Tea Cup", 125.50),
+  createItemObject(9, "Train Plate", trainPlate, "Train Plate", 80.80),
+  createItemObject(10, "Windmill Plate", windmillPlate, "Windmill Plate", 80.80),
 ];
 
 export default data;
