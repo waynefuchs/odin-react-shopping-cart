1b2dab8c841473fdeaba9a9f9e04db66e0e69999
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-10-18 10:49:05 -0400

    Add link to landing page in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ import "./Header.css";
 const Header = () => {
   return (
     <header>
-      <h1>Grantiques</h1>
+      <h1>
+        <Link to="/">Grantiques</Link>
+      </h1>
 
       <Link to="about" className="button">
         About
