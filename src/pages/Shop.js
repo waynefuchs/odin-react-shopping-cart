52ce29d4135dd0d8c53c3d1163a986de48d0bc7f
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-10-18 11:52:13 -0400

    Fix 'Shop' and 'Item' component styling

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,14 +1,15 @@
-import Item from "../components/Item"
-import allItems from '../item-data';
+import Item from "../components/Item";
+import allItems from "../item-data";
+import "./Shop.css";
 
 const Shop = () => {
-
-    return (
-        <div className="shop" data-testid="shop">
-            {allItems.forEach(i => <Item data={i} />)}
-        </div>
-    )
-
-}
+  return (
+    <div className="shop" data-testid="shop">
+      {allItems.map((i) => (
+        <Item data={i} />
+      ))}
+    </div>
+  );
+};
 
 export default Shop;
