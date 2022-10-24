84fbcdec7f649038bce264ef175ad88761d7a139
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-10-24 16:48:34 -0500

    Add CartDisplay component

---
+++ src/components/CartDisplay.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { getAllByRole, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom"; // optional
+import { MemoryRouter as Router } from "react-router-dom";
+
+import Item from "./CartDisplay";
+
+test("Item component exists", () => {
+  render(<CartDisplay />);
+  const element = screen.getByText(/shopping_cart/);
+  expect(element).toBeInTheDocument();
+});
